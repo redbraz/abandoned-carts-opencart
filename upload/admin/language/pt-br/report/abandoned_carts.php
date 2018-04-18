7f08ab9f267d7cc20b88abd837ee08f12a476121
Julio Garcia <redbraz@hotmail.com>
2018-04-18 07:28:19 -0300

    Update abandoned_carts.php
    Correção no idioma Português, para termos mais coerentes.

--- upload/admin/language/pt-br/report/abandoned_carts.php
+++ upload/admin/language/pt-br/report/abandoned_carts.php
@@ -1,25 +1,25 @@
 <?php
 // Heading
 $_['heading_title']        = 'Carrinhos abandonados/pedidos abandonados';
-$_['button_recover']       = 'Enviar mensagem para os carrinhos abandonados/pedidos abandonados';
+$_['button_recover']       = 'Enviar E-mail';
 
 // Text
-$_['text_list']            = 'Lista de carrinhos abandonados/pedidos abandonados';
+$_['text_list']            = 'Listando carrinhos abandonados';
 $_['text_missing']         = 'Pedido abandonado';
-$_['text_search']          = 'If no transactions appear in the customer search, the records counted are bailed carts';
-$_['text_success']         = 'Pronto! Foi enviada uma mensagem para os pedidos de carrinhos abandonados selecionados!';
-$_['text_deleted']         = 'Selected transaction(s) have been deleted';
+$_['text_search']          = 'Se nenhuma transação existir na pesquisa do cliente, os registros são de carrinhos abandonados.';
+$_['text_success']         = 'Sucesso! Um e-mail foi enviado para os clientes selecionados.';
+$_['text_deleted']         = 'As transações selecionadas foram deletadas!';
 
 // Column
 $_['column_order_id']      = 'Número do pedido';
 $_['column_customer']      = 'Cliente';
 $_['column_status']        = 'Status';
-$_['column_date_added']    = 'Data do pedido';
-$_['column_date_modified'] = 'Data de modificação';
+$_['column_date_added']    = 'Data criado';
+$_['column_date_modified'] = 'Data modificado';
 $_['column_abandoned'] 	   = 'Mensagem enviada?';
 $_['column_total']         = 'Total';
 $_['column_action']        = 'Ação';
 
 // Error
-$_['warning_duplicate']    = 'Existem %d pedidos no sistema com o ip de %s. Certifique-se de não enviar mensagens duplicadas, verifique a fila pendente de novos pedidos.';
+$_['warning_duplicate']    = 'Existem %d pedidos no sistema com o ip: %s. Certifique-se para não enviar e-mails duplicados, verifique a fila de novos pedidos.';
 $_['error_permission']     = 'Você não tem permissão para modificar os carrinhos abandonados.';
