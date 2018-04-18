6f5c16a04b4884c26629b12725a3d50200822841
Julio Garcia <redbraz@hotmail.com>
2018-04-18 07:39:11 -0300

    Update abandoned_carts.php

--- upload/admin/language/pt-br/extension/module/abandoned_carts.php
+++ upload/admin/language/pt-br/extension/module/abandoned_carts.php
@@ -13,7 +13,7 @@ $_['text_success']           = 'Configurações salvas com sucesso!';
 // Email Body
 $_['subject_prefix']         = "Seu carrinho ainda esta esperando.";
 $_['failed_cart_greeting']   = 'Olá, %s';
-$_['failed_cart_intro']      = '<h3>Que pena que você não consegui concluir sua compra!<h3><p>Se você esta com dificuldades, deixe-nos saber, para que possamos ajudar a você concluir seu pedido!</p>';
+$_['failed_cart_intro']      = '<h3>Que pena que você não consegui concluir sua compra!</h3><p>Se você esta com dificuldades, deixe-nos saber, para que possamos ajudar a você concluir seu pedido!</p>';
 $_['failed_cart_contents']   = 'Aqui está seu carrinho de compras:';
 $_['failed_cart_body']       = 'Se o seu pedido foi intencional, nós adoraríamos ouvir o seu feedback, sugestões ou reclamações que você possa oferecer para que possamos melhorar nossa loja e a experiência de nossos clientes!';
 $_['failed_cart_footer']     = 'Você recebeu esta mensagem porque você (ou alguém usando seu endereço de e-mail) efetuou pedido mas não finalizou. Você não foi adicionado a nenhum boletim informativo e não receberá mais nossas comunicações como resultado de sua tentativa de compra (a menos que você tenha se inscrito explicitamente para nossa newsletter e/ou notificações de produtos).';
