2b4f1f1f7fe0c4773f80d9fafbd759111b42be2b
Julio Garcia <redbraz@hotmail.com>
2018-04-18 07:38:37 -0300

    Update abandoned_carts.php
    Correção no idioma Português, para termos mais coerentes.

--- upload/admin/language/pt-br/extension/module/abandoned_carts.php
+++ upload/admin/language/pt-br/extension/module/abandoned_carts.php
@@ -4,27 +4,27 @@ $_['heading_title']          = 'Carrinhos Abandonados';
 
 // Text
 $_['text_bailed_carts']      = 'Carrinhos Abandonados';
-$_['text_edit']              = 'Editar opção de Carrinho Abandonado';
+$_['text_edit']              = 'Editar';
 $_['text_extension']         = 'Extenções';
 $_['text_list']              = 'Lista de carrinhos abandonados';
 $_['text_no_results']        = 'Boas notícias! Não há carrinhos abandonados por enquanto.';
-$_['text_success']           = 'Abandoned Carts configuration has been saved!';
+$_['text_success']           = 'Configurações salvas com sucesso!';
 
 // Email Body
-$_['subject_prefix']         = "Ordem incompleta com";
+$_['subject_prefix']         = "Seu carrinho ainda esta esperando.";
 $_['failed_cart_greeting']   = 'Olá, %s';
-$_['failed_cart_intro']      = 'Percebemos que você estava efetuando um pedido em nossa loja recentemente e não concluiu a sua compra; nós só queríamos ter certeza de que você não teve problemas ao finalizar a seu pedido';
-$_['failed_cart_contents']   = 'Aqui está a lista que ficou no seu carrinho de compras:';
-$_['failed_cart_body']       = 'Se o seu pedido foi intencional, nós adoraríamos ouvir o seu feedback, sugestões ou reclamações que você possa oferecer para que possamos melhorar nossa loja e a experiência de nossos cliente!';
+$_['failed_cart_intro']      = '<h3>Que pena que você não consegui concluir sua compra!<h3><p>Se você esta com dificuldades, deixe-nos saber, para que possamos ajudar a você concluir seu pedido!</p>';
+$_['failed_cart_contents']   = 'Aqui está seu carrinho de compras:';
+$_['failed_cart_body']       = 'Se o seu pedido foi intencional, nós adoraríamos ouvir o seu feedback, sugestões ou reclamações que você possa oferecer para que possamos melhorar nossa loja e a experiência de nossos clientes!';
 $_['failed_cart_footer']     = 'Você recebeu esta mensagem porque você (ou alguém usando seu endereço de e-mail) efetuou pedido mas não finalizou. Você não foi adicionado a nenhum boletim informativo e não receberá mais nossas comunicações como resultado de sua tentativa de compra (a menos que você tenha se inscrito explicitamente para nossa newsletter e/ou notificações de produtos).';
 $_['failed_cart_signoff']    = 'De qualquer forma agradecemos pelo interesse em nossos produtos';
-$_['failed_cart_signature']  = 'Atenciosamente,';
+$_['failed_cart_signature']  = '<i>Atenciosamente,</i>';
 
 // Entry
 $_['entry_abandoned_status'] = 'Critérios';
-$_['help_abandoned_status']  = 'Estado da ordem a considerar abandonado';
+$_['help_abandoned_status']  = 'Status do pedido para considerar abandonado';
 $_['entry_limit']            = 'Dia(s) para procurar carrinhos abandonados ';
 $_['entry_status']           = 'Status';
 
 // Button
-$_['button_view']            = 'Visualizar detalhes do pedido abandonado';
+$_['button_view']            = 'Ver detalhes';
